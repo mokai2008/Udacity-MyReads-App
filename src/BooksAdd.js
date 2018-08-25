ec9601c99d67453c6d29cb359ddff11068181ca9
Michael Gad <mokai2008@gmail.com>
2018-08-26 01:19:57 +0200

    constructing BooksAdd

--- src/BooksAdd.js
+++ src/BooksAdd.js
@@ -1,12 +1,56 @@
 import React, {Component} from 'react'
+import * as BooksAPI from './BooksAPI'
+import Books from './Books'
+import {Link} from 'react-router-dom'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 
 class BooksAdd extends Component {
     
+    state = {
+        query = '',
+        Books = []
+    }
+
+    updateQuery = (query) => {
+        this.setState({query: query.trim()})
+    }
+
+    let showingBooks
+    if (this.state.query) {
+        const match = new RegExp(escapeRegExp(this.state.query), 'i')
+        showingBooks = this.state.Books.filter((book) => 
+        match.test(book.title))
+    } else {
+        showingBooks = this.state.Books
+    }
+
+    addBook = (book, shelf) => {
+        this.props.onChange(book, shelf)
+    }
 
     render() {
         return (
-
+            <div className="search-books">
+                <div className="search-books-bar">
+                <Link to = '/' className = 'close-search'>Close</Link>
+                    <div className="search-books-input-wrapper">
+                      <input type="text" placeholder="Search by title or author" 
+                        value = 'this.state.query'
+                        onChange = {(event) => this.updateQuery(event.target.value)}
+                        />
+                </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+              {this.state.Books.map((book, index) => (
+                  <Books books= {book} key = {index} updateShelf = {(shelf) => {
+                      this.addBook(book, shelf)
+                  }}
+              />))}
+            </div>
+          </div>
         )
     }
 }
--- src/BooksList.js
+++ src/BooksList.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import BooksShelf from './BooksShelf';
+import {Link} from 'react-router-dom'
 
 
 class BooksList extends Component {
@@ -27,6 +28,9 @@ class BooksList extends Component {
                         />                                                
                    </div>
                 </div>
+                   <div className="open-search">
+                     <Link to = '/search'> Add a book </Link>
+                  </div>
             </div>
         )
     }
