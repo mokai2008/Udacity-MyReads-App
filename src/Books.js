7e6b4839b1f6af33612c51f30a192b853b24269b
Michael Gad <mokai2008@gmail.com>
2018-08-25 23:59:23 +0200

    Books.js constructed and remove all books from App.js

--- src/Books.js
+++ src/Books.js
@@ -3,10 +3,31 @@ import React, {Component} from 'react'
 
 class Books extends Component {
     
+    changeShelf = (event) => {
+        this.props.updateShelf(event.target.value)
+    }
 
     render() {
         return (
-
+                    <li>
+                        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" 
+                            style={{ width: 128, height: 193, backgroundImage: `url("${this.props.books.imageLinks.thumbnail}")` }}></div>
+                            <div className="book-shelf-changer">
+                              <select onChange = {this.changeShelf} value = {books.shelf}>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{this.props.books.title}</div>
+                          <div className="book-authors">{this.props.books.authors}</div>
+                        </div>
+                    </li>
         )
     }
 }
