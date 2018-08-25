363a8c7bbf3fee9dea5c5a55e6e1e8f931909ed9
Michael Gad <mokai2008@gmail.com>
2018-08-26 00:43:09 +0200

    constructing the BooksList and remove book lists from App.js

--- src/App.js
+++ src/App.js
@@ -38,15 +38,7 @@ class BooksApp extends React.Component {
             </div>
           </div>
         ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-              
-              </div>
-                </div>
+          
                
             <div className="open-search">
               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
--- src/BooksList.js
+++ src/BooksList.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import BooksShelf from './BooksShelf';
 
 
 class BooksList extends Component {
@@ -6,7 +7,27 @@ class BooksList extends Component {
 
     render() {
         return (
-
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                   <div>
+                        <BooksShelf books = {this.props.books.filter((book) =>
+                            (book.shelf === 'currentlyReading'))}
+                            changeShelf ={this.props.books.onChange}
+                        />
+                        <BooksShelf books = {this.props.books.filter((book) =>
+                            (book.shelf === 'read'))}
+                            changeShelf ={this.props.books.onChange}
+                        />
+                        <BooksShelf books = {this.props.books.filter((book) =>
+                            (book.shelf === 'wantToRead'))}
+                            changeShelf ={this.props.books.onChange}
+                        />                                                
+                   </div>
+                </div>
+            </div>
         )
     }
 }
